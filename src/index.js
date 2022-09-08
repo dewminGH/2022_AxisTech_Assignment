1bd181b63b2820f08524c6818327007b1e132e28
trashancsgo@gmail.com <trashancsgo@gmail.com>
2022-09-09 01:32:42 +0530

    wire up redux-thunk and dev_tools

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDom from "react-dom";
 import {Provider} from "react-redux";
-import {createStore} from "redux";
+import {createStore,applyMiddleware,compose} from "redux";
+import thunk from "redux-thunk";
 
 import App from './components/app';
 import Reducers from './reducers/index';
 
+//I only add this to spec my redux store //
+const composeEnhansment = window.__REDUX_DEVTOOLS_EXTENTION_COMPOSE__ || compose;
+
 ReactDom.render(
-    <Provider store={createStore(Reducers)}>
+    <Provider 
+    store={createStore(composeEnhansment(Reducers ,applyMiddleware(thunk)))}>
     <App/>
     </Provider>,
     document.querySelector('#root')
