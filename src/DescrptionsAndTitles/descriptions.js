ec5e89291b6be46859b4af6a2553cbac699839f6
trashancsgo@gmail.com <trashancsgo@gmail.com>
2022-09-09 01:12:31 +0530

    titles/name

---
+++ src/DescrptionsAndTitles/descriptions.js
@@ -0,0 +1,4 @@
+//only use to store some titles and descriptions for the app
+export const TopSegment_title ='We want give them a better tommorow!';
+export const TopSegment_buttonName ='Get Started';
+export const TopSegment_description ='Lorem lpsum is simply dummy text of printing and typeing industry.Lorem lpsum has beeen the industry\'s standerd dummy text ever since the 1500s';
