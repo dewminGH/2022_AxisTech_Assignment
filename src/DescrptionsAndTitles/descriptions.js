cdd2b07aa7fa55123c86ada2e7fb2a6216b9288f
trashancsgo@gmail.com <trashancsgo@gmail.com>
2022-09-09 14:57:22 +0530

    top segment complete

--- src/DescrptionsAndTitles/descriptions.js
+++ src/DescrptionsAndTitles/descriptions.js
@@ -2,3 +2,4 @@
 export const TopSegment_title ='We want give them a better tommorow!';
 export const TopSegment_buttonName ='Get Started';
 export const TopSegment_description ='Lorem lpsum is simply dummy text of printing and typeing industry.Lorem lpsum has beeen the industry\'s standerd dummy text ever since the 1500s';
+export const TopSegment_logo_path=require('../css/logo.png');
--- src/components/TopHeaderSegment.js
+++ src/components/TopHeaderSegment.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 
 import '../css/tophederSegment.css';
 
-const TopHeaderSegment = ( { title , description , buttonName } ) =>{
+class TopHeaderSegment extends Component {
 
+    
 
+    render(){
+        const  { title , description , buttonName , src } = this.props;
     return (
+
     <div className="ui segment topsegment">
-        <h1 className="ui header header"> {title} </h1>
-        <div className="description"> {description} </div>
-        <button className="ui basic button topSegment-button"> {buttonName} </button>
+
+        <div className="ui two column stackable center aligned grid">
+        <div className="middle aligned row">
+
+                    <div className="column">
+                    <img  className='ui medium circular image topsegment-logo'src={src}  
+                    alt="sad Broken </3"  />
+                    </div>
+
+                    <div className="column topsegment-leftCol">
+                    <h1   className="ui header header left floted"> {title} </h1>
+                    <div className="description"> {description} </div>
+                    <button className="ui basic button topSegment-button "> {buttonName} </button>
+                    </div>
+
+         </div>     
+         </div>
     </div>
     )
+    }
 }
 
 export default TopHeaderSegment;
